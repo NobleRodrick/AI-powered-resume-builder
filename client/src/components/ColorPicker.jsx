51e31f55cce239c5bd8c2dd905edd8abcd436842
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2026-03-10 10:01:42 +0100

    fixing the icons

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -32,7 +32,7 @@ const ColorPicker = ({ selectedColor, onChange }) => {
               key={color.value}
               className="relative cursor-pointer group flex flex-col"
               onClick={() => {
-                onChange(color.value);
+                onChange(color.value );
                 setIsOpen(false);
               }}
             >
