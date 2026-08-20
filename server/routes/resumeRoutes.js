6cb5dfae9957989fe60be90e68f5af678c01bf7d
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2026-08-20 02:30:58 +0100

    fixing resume routes

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import protect from "../middlewares/authMiddleware.js"
 import { createResume, deleteResume, getPublicResumeById, getResumeById, updateResume } from "../controllers/resumeController.js"
+import { getUserResumes } from "../controllers/userController.js"
 import upload from "../configs/multer.js"
 
 const resumeRouter = express.Router()
@@ -8,6 +9,8 @@ const resumeRouter = express.Router()
 resumeRouter.post("/create", protect, createResume)
 resumeRouter.put("/update", upload.fields([{ name: "image", maxCount: 1 }, { name: "logo", maxCount: 1 }]), protect, updateResume)
 resumeRouter.delete("/delete/:resumeId", protect, deleteResume)
+resumeRouter.get("/get", protect, getUserResumes)
+resumeRouter.get("/", protect, getUserResumes)
 resumeRouter.get("/get/:resumeId", protect, getResumeById)
 resumeRouter.get("/public/:resumeId", getPublicResumeById)
 
